21e98421ac3f1561efbaa77a8c3270092125dcca
Mika Korkiasaari <mkorkias@gmail.com>
2021-07-20 13:31:08 +0300

    Comments deleted

--- RyhmatyoBuuttiServer/Controllers/UsersController.cs
+++ RyhmatyoBuuttiServer/Controllers/UsersController.cs
@@ -29,10 +29,8 @@ namespace RyhmatyoBuuttiServer.Controllers
         [HttpPost("register")]
         public IActionResult Register(UserRegistrationDTO model, string origin)
         {
-            // List for collecting duplication errors
             List<string> duplicates = new List<string>();
 
-            // Validation if there are duplicates
             if (UserRepository.doesEmailExist(model.Email))
             {
                 duplicates.Add("This email address already exists");
@@ -48,16 +46,12 @@ namespace RyhmatyoBuuttiServer.Controllers
                 return BadRequest(duplicates);
             }
 
-            // Map model to new user object using Automapper
             var user = Mapper.Map<User>(model);
 
-            // Hash password
             user.Password = BC.HashPassword(model.Password);
 
-            // Save account to database
             UserRepository.AddUser(user);
             return Ok(new { message = "Registration successful." });
-
         }
     }
 }
