31562b6cfdafae5b49c76fe870b826f1f3b8c835
Mika Korkiasaari <mkorkias@gmail.com>
2021-09-08 14:00:51 +0300

    GamesController and route for Steam ID added

---
+++ RyhmatyoBuuttiServer/Controllers/GamesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using RyhmatyoBuuttiServer.Models;
+using RyhmatyoBuuttiServer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RyhmatyoBuuttiServer.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class GamesController : ControllerBase
+    {
+        private IUserRepository UserRepository;
+        public GamesController(IUserRepository iUserRepository)
+        {
+            UserRepository = iUserRepository;
+        }
+
+        [HttpPatch("addsteamid")]
+        public IActionResult AddSteamIdToUser(long id, JsonPatchDocument<UserSteamIdDTO> userUpdates)
+        {
+            User user = UserRepository.findUser(id);
+            UserSteamIdDTO steamIdDTO = new UserSteamIdDTO
+            { SteamId = user.SteamId };
+            userUpdates.ApplyTo(steamIdDTO);
+
+            user.SteamId = steamIdDTO.SteamId;
+            UserRepository.UpdateUser(user);
+
+            return Ok(new { message = "Steam ID added to user." });
+        }
+    }
+}
---
+++ RyhmatyoBuuttiServer/Models/UserSteamIdDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RyhmatyoBuuttiServer.Models
+{
+    public class UserSteamIdDTO
+    {
+        public string SteamId { get; set; }
+    }
+}
